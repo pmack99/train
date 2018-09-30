6b438ba9182085b5874cee0736e1c8cd83185901
PMack2 <Prestonmack@gmail.com>
2018-09-30 09:03:11 -0400

    changes js - no more console errors, but no DOM changes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
 var config = {
-    apiKey: "AIzaSyB9c9_fy4pEiVuDq63zW0R4ZJR-PQWOO0M",
-    authDomain: "pmack-train.firebaseapp.com",
-    databaseURL: "https://pmack-train.firebaseio.com",
-    projectId: "pmack-train",
-    storageBucket: "",
-    messagingSenderId: "677237502602"
-  };
-  firebase.initializeApp(config);
-
-
+  apiKey: "AIzaSyB9c9_fy4pEiVuDq63zW0R4ZJR-PQWOO0M",
+  authDomain: "pmack-train.firebaseapp.com",
+  databaseURL: "https://pmack-train.firebaseio.com",
+  projectId: "pmack-train",
+  storageBucket: "",
+  messagingSenderId: "677237502602"
+};
+firebase.initializeApp(config);
 
 var database = firebase.database();
 
@@ -21,76 +19,78 @@ var currentTime = moment();
 var minutesAway = "";
 var nextArrival = "";
 
-$("#submit").on("click", function (event) {
-    event.preventDefault();
+$("#submit").on("click", function(event) {
+  event.preventDefault();
 
-    // Grabbed values from text boxes
-    routeName = $("#routeName").val();
-    destination = $("#destination").val();
-    frequency = $("#frequency").val();
-    firstTrainTime = $("#firstTrain").val(); 
+  // Grabbed values from text boxes
+  routeName = $("#routeName").val();
+  destination = $("#destination").val();
+  frequency = $("#frequency").val();
+  firstTrainTime = $("#firstTrain").val();
 
-    console.log(routeName);
-    console.log(destination);
-    console.log(frequency);
-    console.log(firstTrainTime);
+  console.log(routeName);
+  console.log(destination);
+  console.log(frequency);
+  console.log(firstTrainTime);
+
+  // Code for handling the push
+  database.ref().push({
+    routeName: routeName,
+    destination: destination,
+    frequency: frequency,
+    firstTrainTime: firstTrainTime,
+    time: firebase.database.ServerValue.TIMESTAMP
+    //nextArrival = moment().diff(moment(firstTime), "minutes"),
+    //minutesAway = moment(firstTime, "HH:mm").subtract(1, "years")
+  });
+
+  // ChronoUnit.MONTHS.between(startDate, dateAdded)
+
+  database.ref().on("child_added", function(snapshot) {
+    // storing the snapshot.val() in a variable for convenience
+    var sv = snapshot.val();
+    // Console.loging the last user's data
+    console.log(sv.routeName);
+    console.log(sv.destination);
+    console.log(sv.frequency);
+    console.log(sv.firstTrainTime);
+    //console.log(sv.nextArrival);
+    //console.log(sv.minutesAway);
+
+    // Change the HTML to reflect
+    //$(".table").append(function createRow() {
+      // Create a new table row element
+      //var tRow = $("<tr>");
 
 
-    // Code for handling the push
-    database.ref().push({
-        routeName: routeName,
-        destination: destination,
-        frequency: frequency,
-        firstTrainTime: firstTrainTime,
-        time: firebase.database.ServerValue.TIMESTAMP,
-        //nextArrival = moment().diff(moment(firstTime), "minutes"),
-        //minutesAway = moment(firstTime, "HH:mm").subtract(1, "years")
-        
-    });
+      var newRow = $("<tr>").append(
+        $("<td>").text(routeName),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+       //  $("<td>").text(timeDiff),
+       //  $("<td>").text(minutes)
+    );
 
+    $(".table").append(newRow);
+});
+
+      // Methods run on jQuery selectors return the selector they we run on
+      // This is why we can create and save a reference to a td in the same statement we update its text
+      //var routeN = $("<td>").text(sv.routeName);
+      //var dest = $("<td>").text(sv.destination);
+      //var freq = $("<td>").text(sv.frequency);
+      //var nextA = $("<td>").text(sv.nextArrival);
+      //var minutesA = $("<td>").text(sv.minutesAway);
+
+      // Append the newly created table data to the table row
+      //append(routeN, dest, freq);
+      // Append the table row to the table body
+      //$("tbody").append(tRow);
     
-    // ChronoUnit.MONTHS.between(startDate, dateAdded)
 
-    database.ref().on("child_added", function (snapshot) {
-        // storing the snapshot.val() in a variable for convenience
-        var sv = snapshot.val();
-        // Console.loging the last user's data
-        console.log(sv.routeName);
-        console.log(sv.destination);
-        console.log(sv.frequency);
-        console.log(sv.firstTrainTime);
-        //console.log(sv.nextArrival);
-        //console.log(sv.minutesAway);
-        
+    //createRow();
 
-        // Change the HTML to reflect
-        $("#table").append(
-
-        function createRow () {
-            // Create a new table row element
-            var tRow = $("<tr>");
-
-            // Methods run on jQuery selectors return the selector they we run on
-            // This is why we can create and save a reference to a td in the same statement we update its text
-            var routeN = $("<td>").text(sv.routeName);
-            var dest = $("<td>").text(sv.destination);
-            var freq = $("<td>").text(sv.frequency);
-            //var nextA = $("<td>").text(sv.nextArrival);
-            //var minutesA = $("<td>").text(sv.minutesAway);
-            
-
-            // Append the newly created table data to the table row
-            tRow.append(routeN, dest, freq);
-            // Append the table row to the table body
-            $(".tbody").append(tRow);
-        });
-
-        createRow();
-
-
-
-        // "<div><span>" + snapshot.val().employeeName + "</span><span>" + snapshot.val().role + "</span><span>" + snapshot.val().startDate + "</span><span>" + snapshot.val().monthsWorked + "</span><span>" + snapshot.val().monthlyRate + "</span><span>" + snapshot.val().totalBilled + "</span></div>")
-        // Handle the errors
-    }); 
-    
-})
+    // "<div><span>" + snapshot.val().employeeName + "</span><span>" + snapshot.val().role + "</span><span>" + snapshot.val().startDate + "</span><span>" + snapshot.val().monthsWorked + "</span><span>" + snapshot.val().monthlyRate + "</span><span>" + snapshot.val().totalBilled + "</span></div>")
+    // Handle the errors
+  
+});
